5e87b00e2d9b9cfbbe964b8e9d627bb52ada31b9
guillemmiro <guillem.miro@students.salle.url.edu>
2021-12-21 19:16:47 +0100

    Changes by faozibouybaouenegadrouz

---
+++ src/Business/Article.java
@@ -0,0 +1,8 @@
+package Business;
+
+public class Article extends Trial {
+    private String articleName;
+    private String magazineQuartile;
+    private int acceptProbability;
+    private int denyProbability;
+}
